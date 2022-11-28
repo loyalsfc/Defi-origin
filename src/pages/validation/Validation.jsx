5e5c6189b1902e38bc215d85a9ad20483587f8a8
Olumide Bambe <loyalsfc@gmail.com>
2022-11-28 17:31:10 +0100

    completed sync wallet page

--- src/pages/validation/Validation.jsx
+++ src/pages/validation/Validation.jsx
@@ -7,8 +7,10 @@ function Validation() {
     const walletsDisplay = wallet.map(item=>{
         return(
             <div key={item.id}>
-                <img src={item.image} alt="" />
-                <h5 className=''>{item.name}</h5>
+                <div className='p-[25px] flex items-center justify-center w-full'>
+                    <img src={item.image} alt={item.name.toLocaleLowerCase()} className="w-full h-full max-w-full block" />
+                </div>
+                <h5 className='font-bold'>{item.name}</h5>
             </div>
         )
     })
@@ -20,15 +22,17 @@ function Validation() {
                 </div>
             </header>
             <main className='px-8 pt-[30px] text-[#587087] text-center'>
-                <article className='max-w-[700px] text-center  mx-auto'>
-                    <h2 className='text-[35px] font-medium text-center'>Decentralised Wallet Applications</h2>
-                    <p className='mt-10 text-lg '>Following decentralised finance and blockchain applications support Wallets Validation wallets.</p>
-                </article>
-                <section className='mb-10'>
-                    <div className='overflow-hidden p-6 grid gap-[30px] grid-fit'>
-                        {walletsDisplay}
-                    </div>
-                </section>
+                <div className="max-w-[1064px] mx-auto">
+                    <article className='max-w-[700px] text-center  mx-auto'>
+                        <h2 className='text-[35px] font-medium text-center leading-[150%]'>Decentralised Wallet Applications</h2>
+                        <p className='mt-10 text-lg '>Following decentralised finance and blockchain applications support Wallets Validation wallets.</p>
+                    </article>
+                    <section className='mb-10'>
+                        <div className='overflow-hidden p-6 grid gap-[30px] h-full w-full grid-fit'>
+                            {walletsDisplay}
+                        </div>
+                    </section>
+                </div>
             </main>
         </div>
     )
--- src/pages/validation/wallets.js
+++ src/pages/validation/wallets.js
@@ -21,7 +21,60 @@ import mathwallet from '../../assets/sync_wallet/math-wallet.png'
 import bitpay from '../../assets/sync_wallet/bitpay.jpg'
 import maiar2 from '../../assets/sync_wallet/maiar-2.png'
 import ledgerlive from '../../assets/sync_wallet/ledgerlive.png'
-import walleth from '../../assets/sync_wallet/wallet.png'
+import walleth from '../../assets/sync_wallet/walleth.png'
+import authereum from '../../assets/sync_wallet/authereum.png'
+import huobi from '../../assets/sync_wallet/huobi.jfif'
+import eidoo from '../../assets/sync_wallet/eidoo.png'
+import mykey from '../../assets/sync_wallet/mykey.png'
+import loopstring from '../../assets/sync_wallet/loopstring.jfif'
+import trustvault from '../../assets/sync_wallet/trustvault.png'
+import coin98 from '../../assets/sync_wallet/coin98.png'
+import coolwallet from '../../assets/sync_wallet/coolwallet.png'
+import alice from '../../assets/sync_wallet/alice.png'
+import alphawallet from '../../assets/sync_wallet/alphawallet.jfif'
+import dcentwallet from '../../assets/sync_wallet/dcentwallet.png'
+import zcore from '../../assets/sync_wallet/zelcore.png'
+import nash from '../../assets/sync_wallet/nash.jfif'
+import coinomi from '../../assets/sync_wallet/coinomi.jpg'
+import gridplus from '../../assets/sync_wallet/gridplus.png'
+import cybavowallet from '../../assets/sync_wallet/cybavowallet.png'
+import tokenary from '../../assets/sync_wallet/tokenary.png'
+import wazirx from '../../assets/sync_wallet/wazirx-logo-rounded.png'
+import torus from '../../assets/sync_wallet/torus.jfif'
+import spatium from '../../assets/sync_wallet/spatium.jfif'
+import safePal from '../../assets/sync_wallet/safePal.png'
+import equal from '../../assets/sync_wallet/equal.jfif'
+import infinito from '../../assets/sync_wallet/infinito-wallet.png'
+import walletIo from '../../assets/sync_wallet/wallet.io.png'
+import infinityWallet from '../../assets/sync_wallet/infinity-wallet.png'
+import ownbit from '../../assets/sync_wallet/ownbit.png'
+import easypocket from '../../assets/sync_wallet/easypocket.jpg'
+import bridge from '../../assets/sync_wallet/bridge-wallet.png'
+import sparkpoint from '../../assets/sync_wallet/sparkpoint.png'
+import viawallet from '../../assets/sync_wallet/viawallet.png'
+import bitkeep from '../../assets/sync_wallet/bitkeep.png'
+import vision from '../../assets/sync_wallet/vision.png'
+import swift from '../../assets/sync_wallet/swift.png'
+import peakdefi from '../../assets/sync_wallet/peakdefi.png'
+import cosmosstation from '../../assets/sync_wallet/cosmosstation.png'
+import graph from '../../assets/sync_wallet/graph.jpg'
+import kardachain from '../../assets/sync_wallet/kardachain.png'
+import keplr from '../../assets/sync_wallet/keplr.png'
+import harmony from '../../assets/sync_wallet/harmony.png'
+import iconex from '../../assets/sync_wallet/iconex.png'
+import fetch from '../../assets/sync_wallet/fetch.jpg'
+import xdc from '../../assets/sync_wallet/xdc.png'
+import unstoppable from '../../assets/sync_wallet/unstoppable.png'
+import meetone from '../../assets/sync_wallet/meetone.jpg'
+import dokwallet from '../../assets/sync_wallet/Dokwallet.png'
+import atwallet from '../../assets/sync_wallet/atwallet.png'
+import morixwallet from '../../assets/sync_wallet/morixwallet.png'
+import midas from '../../assets/sync_wallet/midas-wallet.png'
+import ellipal from '../../assets/sync_wallet/ellipal.png'
+import keyringpro from '../../assets/sync_wallet/keyringpro.png'
+import blockchain from '../../assets/sync_wallet/blockchain-logo.png'
+import bsc from '../../assets/sync_wallet/bsc-logo.png'
+import aktionariat from '../../assets/sync_wallet/aktionariat.png'
 
 export default [
     {
@@ -143,5 +196,270 @@ export default [
         id: 24,
         name: "Walleth",
         image: walleth
+    },
+    {
+        id: 25,
+        name: "Authereum",
+        image: authereum
+    },
+    {
+        id: 26,
+        name: "Huobi Wallet",
+        image: huobi
+    },
+    {
+        id: 27,
+        name: "Eidoo",
+        image: eidoo
+    },
+    {
+        id: 28,
+        name: "MYKEY",
+        image: mykey
+    },
+    {
+        id: 29,
+        name: "Loopstring Wallet",
+        image: loopstring
+    },
+    {
+        id: 30,
+        name: "TrustVault",
+        image: trustvault
+    },
+    {
+        id: 31,
+        name: "Coin98",
+        image: coin98
+    },
+    {
+        id: 32,
+        name: "CoolWallet S",
+        image: coolwallet
+    },
+    {
+        id: 33,
+        name: "Alice",
+        image: alice
+    },
+    {
+        id: 34,
+        name: "AlphaWallet",
+        image: alphawallet
+    },
+    {
+        id: 35,
+        name: "D'CENT Wallet",
+        image: dcentwallet
+    },
+    {
+        id: 36,
+        name: "ZelCore",
+        image: zcore
+    },
+    {
+        id: 37,
+        name: "Nash",
+        image: nash
+    },
+    {
+        id: 38,
+        name: "Coinomi",
+        image: coinomi
+    },
+    {
+        id: 39,
+        name: "Gridplus",
+        image: gridplus
+    },
+    {
+        id: 40,
+        name: "CYBAVO Wallet",
+        image: cybavowallet
+    },
+    {
+        id: 41,
+        name: "Tokenary",
+        image: tokenary
+    },
+    {
+        id: 42,
+        name: "Wazirx",
+        image: wazirx
+    },
+    {
+        id: 43,
+        name: "Torus",
+        image: torus
+    },
+    {
+        id: 44,
+        name: "Spatium",
+        image: spatium
+    },
+    {
+        id: 45,
+        name: "SafePal",
+        image: safePal
+    },
+    {
+        id: 46,
+        name: "Equal",
+        image: equal
+    },
+    {
+        id: 47,
+        name: "Infinito",
+        image: infinito
+    },
+    {
+        id: 48,
+        name: "Wallet.io",
+        image: walletIo
+    },
+    {
+        id: 49,
+        name: "Infinity Wallet",
+        image: infinityWallet
+    },
+    {
+        id: 50,
+        name: "Ownbit",
+        image: ownbit
+    },
+    {
+        id: 51,
+        name: "EasyPocket",
+        image: easypocket
+    },
+    {
+        id: 52,
+        name: "Bridge Wallet",
+        image: bridge
+    },
+    {
+        id: 53,
+        name: "SparkPoint",
+        image: sparkpoint
+    },
+    {
+        id: 54,
+        name: "ViaWallet",
+        image: viawallet
+    },
+    {
+        id: 55,
+        name: "BitKeep",
+        image: bitkeep
+    },
+    {
+        id: 56,
+        name: "Vision",
+        image: vision
+    },
+    {
+        id: 57,
+        name: "SWFT Vision",
+        image: swift
+    },
+    {
+        id: 58,
+        name: "PEAKDEFI Wallet",
+        image: peakdefi
+    },
+    {
+        id: 59,
+        name: "Cosmosstation",
+        image: cosmosstation
+    },
+    {
+        id: 60,
+        name: "Graph Protocol",
+        image: graph
+    },
+    {
+        id: 61,
+        name: "KarcaChain",
+        image: kardachain
+    },
+    {
+        id: 62,
+        name: "Keplr",
+        image: keplr
+    },
+    {
+        id: 63,
+        name: "Harmony",
+        image: harmony
+    },
+    {
+        id: 64,
+        name: "ICONex",
+        image: iconex
+    },
+    {
+        id: 65,
+        name: "Fetch",
+        image: fetch
+    },
+    {
+        id: 66,
+        name: "XDC Wallet",
+        image: xdc
+    },
+    {
+        id: 67,
+        name: "Unstoppable Wallet",
+        image: unstoppable
+    },
+    {
+        id: 68,
+        name: "MEET.ONE",
+        image: meetone
+    },
+    {
+        id: 69,
+        name: "Dok Wallet",
+        image: dokwallet
+    },
+    {
+        id: 70,
+        name: "AT.Wallet",
+        image: atwallet
+    },
+    {
+        id: 71,
+        name: "MoriX Wallet",
+        image: morixwallet
+    },
+    {
+        id: 72,
+        name: "Midas Wallet",
+        image: midas
+    },
+    {
+        id: 73,
+        name: "Ellipal",
+        image: ellipal
+    },
+    {
+        id: 74,
+        name: "KEYRING PRO",
+        image: keyringpro
+    },
+    {
+        id: 75,
+        name: "Blockchain",
+        image: blockchain
+    },
+    {
+        id: 76,
+        name: "Binance Smart Chain",
+        image: bsc
+    },
+    {
+        id: 77,
+        name: "Aktionariat",
+        image: aktionariat
     },
 ]
