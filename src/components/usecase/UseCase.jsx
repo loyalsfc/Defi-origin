5c783143c2e035956fdb653627988a146c70c2a8
Olumide Bambe <loyalsfc@gmail.com>
2022-11-29 11:18:05 +0100

    integrated router

--- src/components/usecase/UseCase.jsx
+++ src/components/usecase/UseCase.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function UseCase({iconName, title, content}) {
   return (
@@ -11,7 +12,7 @@ function UseCase({iconName, title, content}) {
                         <h3 className='font-bold text-[1.875rem] my-[1.875rem] leading-[120%]'>{title}</h3>
                         <p className='text-[#B1ADAD text-[15px]'>{content}</p>
                     </div>
-                <button className='py-3 px-6 rounded bg-button text-white text-[15px] w-fit mx-auto font-medium'>Click here</button>
+                <Link to='syncwallet'><button className='py-3 px-6 rounded bg-button text-white text-[15px] w-fit mx-auto font-medium'>Click here</button></Link>
             </div>
         </article>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import Home from './pages/home/Home'
+import Validation from './pages/validation/Validation'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/syncwallet",
+    element: <Validation />
+  }
+])
+  // createRoutesFromElements(
+  //   <Route path='/' exact element={<Home />}>
+  //     <Route path='syncwallet' element={<Validation />} />
+  //   </Route>
+  // )
+
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
